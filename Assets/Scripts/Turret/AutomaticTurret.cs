4c32ba26d82dc950be37c6aca0e52fd74901dabe
Jew <dima.com655@gmail.com>
2022-09-22 15:52:59 +0300

    Add Muzzle Move

--- Assets/Scripts/Turret/AutomaticTurret.cs
+++ Assets/Scripts/Turret/AutomaticTurret.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using DG.Tweening;
 
 public class AutomaticTurret : BaseTurret
 {
@@ -6,14 +7,24 @@ public class AutomaticTurret : BaseTurret
     [SerializeField, Range(2, 40)] protected float _bulletSpeed;
     [SerializeField] protected BaseProjectile _projectilePrefab;
 
+    [SerializeField] private Vector2 gunMove = new Vector2(0.001f, -0.001f);
+    [SerializeField] private Vector2 moveDuration = new Vector2(0.075f, 0.125f);
+
     protected override void Fire()
     {
         base.Fire();
+        HomingProjectile projectile;
+        projectile = Instantiate(_projectilePrefab, _shootPivot[ShootPivotIndex].transform.position, _shootPivot[ShootPivotIndex].transform.rotation) as HomingProjectile;
 
-        HomingProjectile projectile = Instantiate(_projectilePrefab, _shootPivot[ShootPivotIndex].transform.position, _shootPivot[ShootPivotIndex].transform.rotation) as HomingProjectile;
-        projectile.Initialize(_shootPivot[ShootPivotIndex].transform.position, Vector3.zero, _damage, 0f);
-        projectile.SetSpeed(_bulletSpeed);
-        projectile.SetTarget(_currentTarget.transform);
+        _shootPivot[ShootPivotIndex].parent.DOLocalMoveZ(gunMove.y, moveDuration.x).OnComplete(() =>
+        {
+            projectile.Initialize(_shootPivot[ShootPivotIndex].transform.position, Vector3.zero, _damage, 0f);
+            projectile.SetSpeed(_bulletSpeed);
+            projectile.SetTarget(_currentTarget.transform);
+            _shootPivot[ShootPivotIndex].parent.DOLocalMoveZ(gunMove.x, moveDuration.y);
+        });
+
+
         CheckPivotIndex();
     }
 
